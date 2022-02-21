d993fe61eb998f7425efe74a8b19548c2e5609c5
Alejandro <alejandro.rondon@bonitasoft.com>
2022-02-21 18:01:32 +0100

    Changes in POM and SQL queries

---
+++ src/main/resources/openCases.sql
@@ -0,0 +1,9 @@
+select t.bonitaInvoiceID, t.bonitaUserID, t.transactionID, t.registrationNumber, t.transactionDateTime, t.grossAmount, t.netAmount, t.postedDateTime, t.costCentre, t.transactionReason,t.fuelType,t.transactionExplanation,t.status,t.site
+from Transactions t
+where t.status <> 'Completed'
+and t.site = :s
+and ( t.transactionID like :q
+or t.registrationNumber like :q
+or t.costCentre like :q
+or t.status like :q
+)

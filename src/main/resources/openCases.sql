ab84da47a5fea2d4117b1e22aafde261e11d0eba
Alejandro <alejandro.rondon@bonitasoft.com>
2022-02-23 16:42:03 +0100

    update Status attribute for sql

--- src/main/resources/openCases.sql
+++ src/main/resources/openCases.sql
@@ -1,9 +1,9 @@
-select t.bonitaInvoiceID, t.bonitaUserID, t.transactionID, t.registrationNumber, t.transactionDateTime, t.grossAmount, t.netAmount, t.postedDateTime, t.costCentre, t.transactionReason,t.fuelType,t.transactionExplanation,t.status,t.site
+select t.bonitaInvoiceID, t.bonitaUserID, t.transactionID, t.registrationNumber, t.transactionDateTime, t.grossAmount, t.netAmount, t.postedDateTime, t.costCentre, t.transactionReason,t.fuelType,t.transactionExplanation,t.claimStatus,t.site
 from Transactions t
-where t.status <> 'Completed'
+where t.claimStatus <> 'Completed'
 and t.site = :s
 and ( t.transactionID like :q
 or t.registrationNumber like :q
 or t.costCentre like :q
-or t.status like :q
+or t.claimStatus like :q
 )
